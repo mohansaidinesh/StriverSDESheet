c9fec52d5ee764ea75d76551dc47c00e4c641890
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2023-02-12 13:59:26 +0530

    Create mergeTwoSortedArrays.java

---
+++ arrays/mergeTwoSortedArrays.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int i=0;i<n;i++){
+            nums1[m++]=nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}
