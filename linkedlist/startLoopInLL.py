8e7be6c135d8a26f788437eecc93ac56a615de6e
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2023-02-18 15:20:08 +0530

    Create startLoopInLL.py

---
+++ linkedlist/startLoopInLL.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                while slow != head:
+                    slow, head = slow.next, head. next
+                return slow
+        return None
