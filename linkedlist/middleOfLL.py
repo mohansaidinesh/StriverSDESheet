62437df8943808daf4ceb863d3cf833a2c8ae325
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2023-02-18 14:51:48 +0530

    Create middleOfLL.py

---
+++ linkedlist/middleOfLL.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        s=head
+        f=head
+        while f and f.next:
+            s=s.next
+            f=f.next.next
+        return s
