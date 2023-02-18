94286d392f0080021e3e1d36969de1d7ea2ceb40
MOHAN SAI DINESH BODDAPATI <85325733+mohansaidinesh@users.noreply.github.com>
2023-02-18 15:15:53 +0530

    Create intersectionOfTwoLL.py

---
+++ linkedlist/intersectionOfTwoLL.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        f = headA
+        s = headB
+        while f != s:
+            f= headB if f is None else f.next
+            s = headA if s is None else s.next
+        return f
